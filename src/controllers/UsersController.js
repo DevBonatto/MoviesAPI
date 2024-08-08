ebd68f699f2403f064c439d42b16bdf4e73c9e54
Lucca Bonatto <devbonatto@gmail.com>
2024-08-08 11:12:43 -0300

    encoding creating users

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -3,7 +3,6 @@ const knex = require("../database/knex")
 class UsersController {
   static async create(req, res) {
     const { name, email, password } = req.body
-    const { user_id } = req.params
 
     await knex("users").insert({
       name,
