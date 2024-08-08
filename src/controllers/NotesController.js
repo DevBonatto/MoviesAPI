c9fae3175ac86b25ec170387b5f26dd97a20d8ba
Lucca Bonatto <devbonatto@gmail.com>
2024-08-08 18:19:44 -0300

    :sparkles: feature: finishing create notes

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -1,9 +1,27 @@
+const knex = require("../database/knex")
 class NotesController {
   static async create(req, res) {
-    const { title, description, rating } = req.body
+    const { title, description, rating, tags } = req.body
     const { user_id } = req.params
 
-    res.status(200).json({ title, description, rating, user_id })
+    const [ note_id ]  = await knex("movie_notes").insert({
+      title,
+      description,
+      rating,
+      user_id
+    })
+
+    const tagsInsert = tags.map(name => {
+      return {
+        note_id,
+        name,
+        user_id
+      }
+    })
+    
+    await knex("movie_tags").insert(tagsInsert)
+
+    res.status(200).json()
   }
 }
 
