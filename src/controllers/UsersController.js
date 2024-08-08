a5271a98e23b2ae24df165e311d392f2276ff1ed
Lucca Bonatto <devbonatto@gmail.com>
2024-08-08 14:39:26 -0300

    :sparkles: feature: adding hash password & checking email

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,13 +1,25 @@
 const knex = require("../database/knex")
 const AppError = require("../utils/appError")
+const { hash } = require("bcryptjs")
 class UsersController {
   static async create(req, res) {
     const { name, email, password } = req.body
 
+    const hashedPassword = await hash(password, 8)
+    
+    const thisEmailAlreadyExists = await knex("users")
+      .select("*")
+      .where({ email })
+      .first()
+
+    if(thisEmailAlreadyExists){
+      throw new AppError("Esse email já está cadastrado no sistema!")
+    }
+      
     await knex("users").insert({
       name,
       email,
-      password
+      password: hashedPassword
     })
 
     res.status(201).json( { name, email, password } )
